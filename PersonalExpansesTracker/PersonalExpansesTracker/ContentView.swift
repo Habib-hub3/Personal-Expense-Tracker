76c5bf5e18c3409ae050e31b94d7afd7c83dac85
Habib-hub3 <202200123@student.polytechnic.bh>
2026-08-02 15:00:48 +0300

    Start with the project

---
+++ PersonalExpansesTracker/PersonalExpansesTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PersonalExpansesTracker
+//
+//  Created by Habib Alshoofa on 02/08/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PersonalExpansesTracker/PersonalExpansesTracker/PersonalExpansesTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalExpansesTrackerApp.swift
+//  PersonalExpansesTracker
+//
+//  Created by Habib Alshoofa on 02/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct PersonalExpansesTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
