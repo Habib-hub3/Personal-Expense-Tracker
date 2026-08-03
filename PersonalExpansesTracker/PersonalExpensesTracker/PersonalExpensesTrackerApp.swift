6967e8e16417916832f9bbf9d58919b9997a7011
Habib-hub3 <202200123@student.polytechnic.bh>
2026-08-03 16:45:11 +0300

    Edited names and Prototype Sign in page

--- PersonalExpansesTracker/PersonalExpensesTracker/PersonalExpensesTrackerApp.swift
+++ PersonalExpansesTracker/PersonalExpensesTracker/PersonalExpensesTrackerApp.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @main
-struct PersonalExpansesTrackerApp: App {
+struct PersonalExpensesTrackerApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
